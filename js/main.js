45435b9a131d7a752c49fef4d2ad0d8305d0ff50
Kamola0911 <kamola092002@mail.ru>
2023-05-04 16:34:12 +0500

    first commit

---
+++ js/main.js
@@ -0,0 +1,27 @@
+let modeBtn = document.getElementById("dark-light");
+
+modeBtn.addEventListener("click", function () {
+  if (document.body.className != "dark") {
+    this.firstElementChild.src = "images/light.svg";
+  } else {
+    this.firstElementChild.src = "images/dark.svg";
+  }
+  document.body.classList.toggle("dark");
+});
+
+$('.owl-carousel').owlCarousel({
+    loop:true,
+    margin:10,
+    nav:true,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:3
+        },
+        1000:{
+            items:5
+        }
+    }
+})
